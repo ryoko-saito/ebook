59730783fae80e8e7f34dd804a4ca42126a9373b
Ryoko Saito <ryoko@saitodev.co>
2018-05-17 09:56:23 +0900

    first commit

---
+++ func.go
@@ -0,0 +1,31 @@
+package ebook
+
+import (
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func SaveImage(url string) error {
+	resp, err := http.Get(url)
+	defer resp.Body.Close()
+	if err != nil {
+		return err
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	n := strings.LastIndex(url, "/")
+	p := url[n+1:]
+
+	file, err := os.Create(p)
+	defer file.Close()
+	if err != nil {
+		return err
+	}
+	file.Write(body)
+
+	return nil
+}
